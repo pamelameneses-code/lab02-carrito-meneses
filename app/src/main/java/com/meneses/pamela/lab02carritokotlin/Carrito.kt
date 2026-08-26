6bd465b875910f59e3d2952c0d1c9af63a3b33c9
Pamela <pamela.meneses@tecsup.edu.pe>
2026-08-25 23:47:37 -0500

    Proyecto inicial con menú del carrito

---
+++ app/src/main/java/com/meneses/pamela/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,9 @@
+package com.meneses.pamela.lab02carritokotlin
+
+import java.util.Locale
+
+fun main() {
+    println("=========================================")
+    println("    CARRITO DE COMPRAS - TIENDA TECSUP   ")
+    println("=========================================")
+}
