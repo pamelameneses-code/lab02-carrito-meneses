e81dc3119feaedf7b31160d32c0f8755aa01c22f
Pamela Meneses <pamela.meneses@tecsup.edu.pe>
2026-08-30 18:28:44 -0500

    Agrega validación de cuotas y tasas de interés

--- Semana02/CalculadoraCuotas/app/src/main/java/com/meneses/pamela/calculadoracuotas/Main.kt
+++ Semana02/CalculadoraCuotas/app/src/main/java/com/meneses/pamela/calculadoracuotas/Main.kt
@@ -9,3 +9,16 @@ data class Producto(
 fun calcularMontoInicial(producto: Producto): Double {
     return producto.precio * producto.cantidad
 }
+
+fun validarCuotas(cuotas: Int): Boolean {
+    return cuotas == 6 || cuotas == 12 || cuotas == 24
+}
+
+fun obtenerTasaInteres(cuotas: Int): Double {
+    return when (cuotas) {
+        6 -> 0.20
+        12 -> 0.40
+        24 -> 0.60
+        else -> 0.0
+    }
+}
