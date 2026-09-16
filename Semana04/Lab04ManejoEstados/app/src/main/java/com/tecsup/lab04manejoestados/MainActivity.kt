a23cd4d5c9ae69c0f4f4f2cd2d8c5e2e33c554ba
Pamela Meneses <pamela.meneses@tecsup.edu.pe>
2026-09-15 21:36:00 -0500

    Agrega botones Subir y Bajar para modificar la temperatura

--- Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04manejoestados/MainActivity.kt
+++ Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04manejoestados/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,10 +38,17 @@ class MainActivity : ComponentActivity() {
 fun TemperatureDisplay(modifier: Modifier = Modifier) {
     var temperatura by remember { mutableStateOf(20) }
 
-    Text(
-        text = "Temperatura: $temperatura°",
-        modifier = modifier
-    )
+    Column(modifier = modifier) {
+        Text(text = "Temperatura: $temperatura°")
+        Row {
+            Button(onClick = { temperatura++ }) {
+                Text("Subir")
+            }
+            Button(onClick = { temperatura-- }) {
+                Text("Bajar")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
