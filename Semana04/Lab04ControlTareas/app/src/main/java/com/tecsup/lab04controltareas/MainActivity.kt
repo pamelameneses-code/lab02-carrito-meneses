d206e9f7ea1f9f39727f85f5b7f924a0b1992332
Pamela Meneses <pamela.meneses@tecsup.edu.pe>
2026-09-15 23:54:54 -0500

    Actualiza modelo Tarea con id, nombre y estado completada

--- Semana04/Lab04ControlTareas/app/src/main/java/com/tecsup/lab04controltareas/MainActivity.kt
+++ Semana04/Lab04ControlTareas/app/src/main/java/com/tecsup/lab04controltareas/MainActivity.kt
@@ -6,42 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.tecsup.lab04controltareas.ui.theme.Lab04ControlTareasTheme
+
+data class Tarea(
+    val id: Int,
+    val nombre: String,
+    val completada: Boolean = false
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Lab04ControlTareasTheme {
+            MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Text(
+                        text = "Total de tareas: 0",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab04ControlTareasTheme {
-        Greeting("Android")
     }
 }
