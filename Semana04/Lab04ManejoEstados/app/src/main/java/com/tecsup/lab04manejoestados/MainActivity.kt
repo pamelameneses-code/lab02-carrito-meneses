0ff7f6bac1c735a5eb64aa47ba945d04b77f5107
Pamela Meneses <pamela.meneses@tecsup.edu.pe>
2026-09-15 21:31:02 -0500

    Crea composable TemperatureDisplay con estado inicial en 20

---
+++ Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04manejoestados/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.tecsup.lab04manejoestados
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tecsup.lab04manejoestados.ui.theme.Lab04ManejoEstadosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab04ManejoEstadosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TemperatureDisplay(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TemperatureDisplay(modifier: Modifier = Modifier) {
+    var temperatura by remember { mutableStateOf(20) }
+
+    Text(
+        text = "Temperatura: $temperatura°",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TemperatureDisplayPreview() {
+    Lab04ManejoEstadosTheme {
+        TemperatureDisplay()
+    }
+}
