1de033778e04580a18887ea8ab73ea458f593219
Pamela Meneses <pamela.meneses@tecsup.edu.pe>
2026-09-22 13:59:30 -0500

    Actualizacion de compileSdk a 37 e integracion de AppNavigation en MainActivity

--- Semana05/Lab05Navegacion/app/src/main/java/com/example/semana05_navegacion/MainActivity.kt
+++ Semana05/Lab05Navegacion/app/src/main/java/com/example/semana05_navegacion/MainActivity.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import com.example.semana05_navegacion.navigation.AppNavigation
-import com.example.semana05_navegacion.ui.theme.Semana05_NavegacionTheme
+import com.example.semana05_navegacion.ui.theme.Lab05NavegacionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Semana05_NavegacionTheme {
-                AppNavigation()
+            Lab05NavegacionTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
             }
         }
     }
