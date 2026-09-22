3e56396faefeb5a0391a7abec6d35fea3d410a12
Pamela Meneses <pamela.meneses@tecsup.edu.pe>
2026-09-22 14:04:31 -0500

    Actualizado nombre de usuario a Pamela Meneses Mayhua en ProfileScreen

--- Semana05/Lab05Navegacion/app/src/main/java/com/example/semana05_navegacion/screens/ProfileScreen.kt
+++ Semana05/Lab05Navegacion/app/src/main/java/com/example/semana05_navegacion/screens/ProfileScreen.kt
@@ -24,7 +24,7 @@ fun ProfileScreen(navController: NavController) {
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
-            text = "Juan León Suiyon",
+            text = "Pamela Meneses Mayhua ",
             style = MaterialTheme.typography.bodyLarge,
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
